bb805a67ab45b9fd05206cb9aa2879b516a2ab02
Ando <ando@gap600.com>
2023-05-24 12:11:11 +0400

    added makePayment function

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const userRoles = [
   { id: 1, role: 'user' },
   { id: 2, role: 'member' },
@@ -25,5 +27,41 @@ module.exports.jsonParser = function (arg) {
   } catch (e) {
     console.error('jsonParser error => ', e.message);
     return null;
+  }
+};
+
+module.exports.makePayment = async function (
+  cookie,
+  toUserId,
+  amount,
+  description
+) {
+  try {
+    const mainAppUrl = process.env.MAIN_APP_URL;
+    if (!mainAppUrl)
+      return {
+        success: false,
+        message: 'Main app URL not defined',
+      };
+
+    const response = await axios({
+      method: 'post',
+      url: `${mainAppUrl}/payment-srv/payment/balance`,
+      headers: {
+        Cookie: cookie, // Pass the active cookie session from the incoming request
+        'is-internal': true,
+      },
+      data: {
+        toUserId,
+        amount,
+        description,
+      },
+    });
+    console.log('response ', response.data);
+
+    return response.data;
+  } catch (e) {
+    console.log(e.response.data);
+    return { success: false, ...e.response.data };
   }
 };
