94b5713d19fc6f7ef0dd4daf59b3eba3561c783b
Ando <ando@gap600.com>
2023-12-20 20:30:47 +0400

    upgrade media, changed checkPermission to check more than one permission

--- middlewares/checkPermission.js
+++ middlewares/checkPermission.js
@@ -3,13 +3,22 @@ const { PermissionError } = require('../errors');
 /**
  * Middleware to check current signed user permission by permission name
  * throws permission error if not has access, otherwise goes to next middleware
- * @param permissionName {string} - name of permission
+ * @param permission {string|Array<string>} - name of permission(s)
  * @returns {function(*, *, *): Promise<*>}
  */
-function checkPermission(permissionName) {
+function checkPermission(permission) {
   return async function (req, res, next) {
-    if (!req.user?.permissions?.includes(permissionName))
+    if (!req.user?.permissions || !Array.isArray(req.user.permissions))
       throw new PermissionError('Permission denied');
+
+    if (Array.isArray(permission)) {
+      for (let perm of permission) {
+        if (req.user.permissions.includes(perm)) return next();
+      }
+      throw new PermissionError('Permission denied');
+    } else if (!req.user?.permissions?.includes(permission))
+      throw new PermissionError('Permission denied');
+
     return next();
   };
 }
