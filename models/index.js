c422eb6b6089272780a664b0c7e62e1066fe35b3
Harshit <develop@enkompass.net>
2024-10-20 00:55:28 +0530

    redis ssl connection enabled

--- models/index.js
+++ models/index.js
@@ -7,19 +7,19 @@ const Sequelize = require('sequelize');
 const pg = require('pg');
 const basename = path.basename(__filename);
 const config = {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.SHARED_DATABASE_NAME,
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    dialect: process.env.DATABASE_DIALECT,
-    dialectModule: pg,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // disable SSL verification
-      },
-    }
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.SHARED_DATABASE_NAME,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  dialect: process.env.DATABASE_DIALECT,
+  dialectModule: pg,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // disable SSL verification
+    },
+  },
 };
 
 if (!process.env.MYFANZ_DIALECT_OPTIONS) {
